f1283e93df5d9d021f4b53c7ce19013539cc5ffd
Luis Manuel Deras <lmrderas@gmail.com>
2015-04-21 21:16:00 -0600

    Update Insert inicial.sql

--- Scripts/DML/Insert inicial.sql
+++ Scripts/DML/Insert inicial.sql
@@ -51,6 +51,20 @@ INSERT INTO TIPO_MEDICAMENTO(descripcion)
 VALUES('Mucolíticos');
 
 
+-- Roles
+
+INSERT INTO ROLES(Nombre, Descripcion)
+VALUES('Médico', 'Este usuario tiene permisos para ver las pantallas correspondientes a sus citas, pacientes');
+
+INSERT INTO ROLES(Nombre, Descripcion)
+VALUES('Administrador', 'Este usuario tiene permisos para crear usuarios, otorgar roles a usuario, actualizar información de usuarios, ver reporterías');
+
+INSERT INTO ROLES(Nombre, Descripcion)
+VALUES('Personal de farmacia', 'Tiene permisos para ver inventario de farmacia, agregar, modificar y revocar medicamentos, otorgar medicamentos por prescripcion');
+
+INSERT INTO ROLES(Nombre, Descripcion)
+VALUES('Personal de registro', 'Tiene permisos para registrar pacientes, actualizar información de pacientes y generar cita');
+
 
 
 
